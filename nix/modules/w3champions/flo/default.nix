997242ff68216911868f36a320da91709320d0eb
Clemens Horn <clemens.horn@mni.thm.de>
2025-04-17 11:41:51 +0200

    fix(nix): add opt for flo

--- nix/modules/w3champions/flo/default.nix
+++ nix/modules/w3champions/flo/default.nix
@@ -12,6 +12,13 @@
   cfg = config.w3champions.flo;
 in {
   imports = [inputs.flo.nixosModules.flo];
+  options = {
+    w3champions = {
+      flo = {
+        enable = lib.mkEnableOption "Enable W3Champions FLO" // {default = false;};
+      };
+    };
+  };
   config = lib.mkIf (cfg.enable) {
     services = {
       flo = {
